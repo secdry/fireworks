7c1ef080eddca6574cf0a3cbb3555be80c2c6f1b
secdry <98429382+secdry@users.noreply.github.com>
2023-02-10 18:21:44 -0300

    The site upload

---
+++ type.js
@@ -0,0 +1,15 @@
+var content = 'FESTA!!!';
+var text = document.getElementById('text');
+
+var speed = 200;
+var cont = 0;
+
+function typeWriter () {
+  if(cont < content.length){
+    text.textContent += content.charAt(cont);
+    cont++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
